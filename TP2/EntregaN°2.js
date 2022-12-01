e7cd043dbf9bc28cb61da9aee856f2099a31712a
Ssanti66 <santiagosaucedo66@gmail.com>
2022-12-01 17:14:13 -0300

    update

--- TP2/EntregaN°2.js
+++ TP2/EntregaN°2.js
@@ -2,12 +2,16 @@ const fs = require ("fs");
 
 class productManager{
 
-    constructor(){
-        this.products = []
+    constructor(path){        
         this.path = path
     }
 
     
+    getNextId = (list) =>{
+        const count = list.length
+        return (count >0) ? list[count-1].id + 1 : 1
+    }
+
     addProduct = async(title, description, price, thumbnail, code, stock) => 
     {
 
@@ -21,21 +25,20 @@ class productManager{
                 const validate = listaDeProductos.find(codigo => codigo.code == code)
 
                 if(!validate){
-                    let idProducto = listaDeProductos[listaDeProductos.length - 1].id + 1;
-                    let nuevoProducto = {id:idProducto, title, description, price, thumbnail, code, stock};
+                    let id = this.getNextId(listaDeProductos)
+                    const nuevoProducto = {id, title, description, price, thumbnail, code, stock};
                     listaDeProductos.push(nuevoProducto)
-                    await fs.promises.appendFile(this.path, JSON.stringify(listaDeProductos))
+                    await fs.promises.writeFile(this.path, JSON.stringify(listaDeProductos))
+                    console.log("El producto se ha ingresado correctamente");
 
                 }else{console.log("El producto ya fue ingresado");}             
             }if(!fs.existsSync(this.path)) { 
-            const id = 1
-            const nuevoProducto = {id, title, description, price, thumbnail, code, stock};
-            this.products.push(nuevoProducto)
-            await fs.promises.writeFile(this.path, JSON.stringify(this.products))
+            const nuevoProducto = [{id:1, title, description, price, thumbnail, code, stock}];
+            await fs.promises.writeFile(this.path, JSON.stringify(nuevoProducto))
             }
-            console.log("El producto se ha ingresado correctamente");
 
     }
+            
 
     getProduct = () => {
         const listadoDeProductos = JSON.parse(fs.readFileSync(this.path, "utf-8"))       
@@ -58,33 +61,30 @@ class productManager{
         const resultado = listadoDeProductos.filter(producto => producto.id != id)
         fs.writeFileSync(this.path, JSON.stringify(resultado))
 
-        }else{console.log("el producto no existe");}}
-
-    updateProduct = (id, campo) =>{
-        const listadoDeProductos = JSON.parse(fs.readFileSync(this.path, "utf-8"))
-        const producto = listadoDeProductos.find(producto => producto.id == id)
-        if ((producto)){
-            const dato = prompt("Dato a actualizar")    
-            if(campo == "title"){
-            listadoDeProductos[id-1].title = dato}
-            if(campo == "description"){
-            listadoDeProductos[id-1].description = dato}
-            if(campo == "price"){ 
-            listadoDeProductos[id-1].price = dato}
-            if(campo == "thumbnail"){ 
-            listadoDeProductos[id-1].thumbnail = dato}
-            if(campo == "code"){ 
-            listadoDeProductos[id-1].code = dato}
-            if(campo == "stock"){ 
-                listadoDeProductos[id-1].stock = dato}
-                
-                fs.writeFileSync(this.path, JSON.stringify(listadoDeProductos))
-            }else{console.log("No existe el producto");}
-        }
+        }else{console.log("el producto no existe");}
     }
 
+    updateProduct = (id, obj) =>{
+
+        obj.id = id
+        const listadoDeProductos = JSON.parse(fs.readFileSync(this.path, "utf-8"))
+
+        for (let i=0; i<listadoDeProductos.length; i++){
+            if(listadoDeProductos[i].id == id){
+                listadoDeProductos[i] = obj
+                break
+            }
+        }        
+        fs.writeFileSync(this.path, JSON.stringify(listadoDeProductos))
+
+    }
+}
     
-    const path = "./listadoProductos.json"
-    const allProducts = new productManager(path);
-    allProducts.addProduct("remera","sin descripcion", "200", "sin imagen", "123", 25);
-  
+const path = "./listadoProductos.json"
+const allProducts = new productManager(path);
+
+allProducts.addProduct("zapatilla", "sin descripcion", "$4000", "sin imagen", "123", 300)
+
+      
+
+
